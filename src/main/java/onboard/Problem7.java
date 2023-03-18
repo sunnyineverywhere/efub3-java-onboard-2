079216b66904d0118588abf66148029fd0938c27
YEJI <yeji.lee.kim@gmail.com>
2023-03-19 00:38:17 +0900

    feat: solve Problem7

--- src/main/java/onboard/Problem7.java
+++ src/main/java/onboard/Problem7.java
@@ -2,6 +2,7 @@ package onboard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * 백준 10798
@@ -11,7 +12,21 @@ import java.util.List;
  */
 public class Problem7 {
     public static String solution(List<String> values){
+        int max = values.get(0).length();
+        for(int i = 1; i < values.size(); i++)
+            if(max < values.get(i).length())
+                max = values.get(i).length();
+
         String answer = "";
+
+        for(int j = 0; j < max; j++) {
+            for (int i = 0; i < values.size(); i++) {
+                if(values.get(i).length() <= j)
+                    continue;
+
+                answer = answer.concat(values.get(i).substring(j, j + 1));
+            }
+        }
         return answer;
     }
 }
