77bdf8a6be6dc9dac19a74d67dfd3231cbe0973c
chhaewxn <chaewon1019@ewhain.net>
2023-03-20 02:56:23 +0900

    ✨ feat: solve Problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,33 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(Integer[] line : values) {
+            int count = 0;
+            for(Integer value : line) {
+                if(value == 1) {
+                    count++;
+                }
+            }
+            String result = "";
+            switch(count) {
+                case 0:
+                    result = "D";
+                    break;
+                case 1:
+                    result = "C";
+                    break;
+                case 2:
+                    result = "B";
+                    break;
+                case 3:
+                    result = "A";
+                    break;
+                case 4:
+                    result = "E";
+                    break;
+            }
+            answer.add(result);
+        }
         return answer;
     }
 }
