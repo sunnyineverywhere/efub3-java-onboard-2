f85adc535d88a0668cd6ec7a3ad5e221291c5ea5
Soyeon-Cha <7103sy@naver.com>
2023-03-20 02:43:46 +0900

    ✨ feat : solve Problem1

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,30 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+
+        Integer sum = 0;
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                sum += values[i][j];
+            }
+            if (sum==3) {
+                answer.add(i,"A");
+            }
+            else if (sum==2) {
+                answer.add(i,"B");
+            }
+            else if (sum==1) {
+                answer.add(i,"C");
+            }
+            else if (sum==0) {
+                answer.add(i,"D");
+            }
+            else if (sum==4) {
+                answer.add(i,"E");
+            }
+            sum = 0;
+        }
         return answer;
     }
 }
