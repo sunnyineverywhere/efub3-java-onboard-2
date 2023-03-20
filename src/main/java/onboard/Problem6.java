d8e638b69e01d9f2db210f4171ebe31e367736cf
june0216 <ziyun1612@ewhain.net>
2023-03-20 17:12:25 +0900

    :sparkles: solve Problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -1,6 +1,8 @@
 package onboard;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,6 +14,17 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(Integer[] val : values)
+        {
+            int sum = Arrays.stream(val).reduce(0,(a,b)->  a + b);
+            if(sum == 4)
+            {
+                answer.add("E");
+            }
+            else{
+                answer.add(String.valueOf((char)(68 - sum)));
+            }
+        }
         return answer;
     }
 }
