f4e267e9728d507d942f246fd35855a998507f52
Soyeon-Cha <7103sy@naver.com>
2023-03-20 21:20:55 +0900

    ✨ feat : solve Problem8

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -8,9 +8,30 @@ import java.util.List;
  * 실버 5
  * link : https://www.acmicpc.net/problem/2563
  */
-public class Problem8 {
+public class Problem8 { // 0~99, 0~99
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+        int[][] colored = new int[100][100];
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
+                colored[i][j] = 0;
+            }
+        }
+
+        for(int i=0; i<paperNumber; i++){
+            for(int j=locations[i][0]; j<(locations[i][0]+10); j++){
+                for(int k=locations[i][1]; k<(locations[i][1]+10); k++){
+                    colored[j][k] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
+                answer += colored[i][j];
+            }
+        }
+
         return answer;
     }
 }
