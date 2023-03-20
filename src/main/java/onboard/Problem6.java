996776b910ad7035a615ad823e10cc1b81e002d4
Min A <min2010011@gmail.com>
2023-03-20 23:40:39 +0900

    feat: Problem6 풀이 추가

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,16 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        String[] results = {"E", "A", "B", "C", "D"};
+        for (Integer[] value : values) {
+            int cnt = 0;    // 배의 개수
+            for (Integer x : value) {
+                if (x == 0) {
+                    cnt++;
+                }
+            }
+            answer.add(results[cnt]);
+        }
         return answer;
     }
 }
