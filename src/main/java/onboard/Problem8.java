af305ffadaa1cc898d947a369e8e9fa786ff7c88
chhaewxn <chaewon1019@ewhain.net>
2023-03-20 16:28:59 +0900

    ✨ feat: solve Problem8

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -10,7 +10,21 @@ import java.util.List;
  */
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
-        Integer answer = 0;
+        boolean[][] board = new boolean[100][100]; // 100X100 크기의 보드 생성
+
+        Integer answer = 0; // 종이의 면적을 더할 변수 초기화
+        for (int i=0; i< paperNumber; i++) {
+            int x = locations[i][0]; // 종이의 x 좌표
+            int y = locations[i][1]; // 종이의 y 좌표
+            for (int j=x; j<x+10; j++) { //10x10 크기의 영역에 대해
+                for (int k =y; k<y+10; k++) {
+                    if(!board[j][k]) { // 종이가 없다면
+                        board[j][k] = true; // 종이 붙이고
+                        answer++; // 그 면적 더하기
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
