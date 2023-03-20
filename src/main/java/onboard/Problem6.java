c6cc573715774e2e6f17a3954f4ce179c72e4d56
hannah0226 <mohan0226@naver.com>
2023-03-21 04:11:06 +0900

    ✨ feat: Problem6 해결

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,24 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+
+        for(int i=0;i<3;i++) {
+            int one=0;
+            for(int j=0;j<4;j++) {
+                if(values[i][j]==1)
+                    one++;
+            }
+            if(one==0)
+                answer.add("D");
+            else if(one==1)
+                answer.add("C");
+            else if(one==2)
+                answer.add("B");
+            else if(one==3)
+                answer.add("A");
+            else
+                answer.add("E");
+        }
         return answer;
     }
 }
