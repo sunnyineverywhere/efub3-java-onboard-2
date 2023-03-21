99ed3c9620ae279cf77058e55bc4bb91cc1ead90
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-21 13:38:51 +0900

    Problem7 solved

--- src/main/java/onboard/Problem7.java
+++ src/main/java/onboard/Problem7.java
@@ -12,6 +12,22 @@ import java.util.List;
 public class Problem7 {
     public static String solution(List<String> values){
         String answer = "";
+        String[] sol= {values.get(0),values.get(1),values.get(2),values.get(3),values.get(4)};
+	        int max=sol[0].length();
+	        //최대 길이 구하기
+	        for(int i=0;i<5;i++) {
+	        	if(max<sol[i].length()) max=sol[i].length();
+	        }
+	        
+	        for(int i=0;i<max;i++) {
+	        	for(int j=0;j<5;j++) {
+	        		//배열의 길이가 충분한 경우
+	        		if(sol[j].length()>i) {
+	        		char c=sol[j].charAt(i);
+	        		answer+=c;
+	        		}
+	        	}
+	        }
         return answer;
     }
 }
