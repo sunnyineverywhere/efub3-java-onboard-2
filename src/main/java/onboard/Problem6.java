a5b05a5742ddcc5fc7a522d1bf99f8bdb8ab08dd
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-21 12:50:13 +0900

    Problem6 solved

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,17 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(int i=0;i<3;i++) {
+        	int zero=0;
+        	for(int j=0;j<4;j++) {
+        		if(values[i][j]==0) zero++;
+        	}
+        	if(zero==1) answer.add("A");
+        	else if(zero==2) answer.add("B");
+        	else if(zero==3) answer.add("C");
+        	else if(zero==4) answer.add("D");
+        	else answer.add("E");
+        }
         return answer;
     }
 }
