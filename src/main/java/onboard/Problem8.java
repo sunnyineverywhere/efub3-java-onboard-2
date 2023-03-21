2434398079ad38f95e3c78112ad8d73b69a0a42d
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-21 14:43:53 +0900

    Problem8 solved

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -11,6 +11,33 @@ import java.util.List;
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+        Integer[][] paper= new Integer[100][100];
+	    for(int i=0;i<100;i++) {
+	    	for(int j=0;j<100;j++) {
+	    		paper[i][j]=0;	    	   
+	    		}
+	    }
+	       
+	    //검은 색종이의 수 많큼 붙이기
+	    for(int i=0;i<paperNumber;i++) {
+	    	int x=locations[i][0];
+	    	int y=100-locations[i][1];
+	    	   
+	    	  
+	    for(int j=y ; j>y-10 ; j--) {
+	        for(int k=x ; k<x+10 ; k++) {
+	    			paper[j][k]++;
+	    		}
+	    	}
+	    }
+	       
+	    for(int i=0;i<100;i++) {
+	    	for(int j=0;j<100;j++){
+	    		if(paper[i][j]!=0){
+	    			answer=answer+1;
+	    		   }
+	    	   }
+	       }
         return answer;
     }
 }
