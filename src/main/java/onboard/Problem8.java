4f3d92118e17f6f4da31d5d2ee393941df48f512
ParkIsComing <maroony55@gmail.com>
2023-03-23 08:44:46 +0900

    ✨ feat: Problem8 풀이 추가

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -1,6 +1,5 @@
 package onboard;
 
-import java.util.List;
 
 /**
  * 백준 2563
@@ -11,6 +10,18 @@ import java.util.List;
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+        for(int i=0; i<paperNumber; i++){
+            answer += 10 *10;
+            if(i==0){
+                continue;
+            }
+            for(int j=0; j<i; j++){
+                if(Math.abs(locations[j][0]-locations[i][0])<10 && Math.abs(locations[j][1] - locations[i][1])<10){
+                    int overlap = (10-Math.abs(locations[j][0]-locations[i][0])) * (10-Math.abs(locations[j][1]-locations[i][1]));
+                    answer -= overlap;
+                }
+            }
+        }
         return answer;
     }
 }
