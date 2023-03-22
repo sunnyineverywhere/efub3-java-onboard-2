a55361ad9b85173c2312c397e38a16a0bb4f5415
Minseo Cho <min1130@ewhain.net>
2023-03-23 05:30:53 +0900

    ✨ feat: solve Problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,28 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+
+        for (int i=0; i<3; i++) {
+            int sum=0;
+
+            for (int j=0; j<4; j++) {
+                sum += values[i][j];
+            }
+
+            switch (sum) {
+                case 0: answer.add("D");
+                        break;
+                case 1: answer.add("C");
+                        break;
+                case 2: answer.add("B");
+                        break;
+                case 3: answer.add("A");
+                        break;
+                case 4: answer.add("E");
+                        break;
+            }
+        }
+
         return answer;
     }
 }
