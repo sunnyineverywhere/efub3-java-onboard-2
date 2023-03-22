b4073bfa5c03cabe5fd3cdb425d66124116d3e3e
ParkIsComing <maroony55@gmail.com>
2023-03-23 08:44:29 +0900

    ✨ feat: Problem7 풀이 추가

--- src/main/java/onboard/Problem7.java
+++ src/main/java/onboard/Problem7.java
@@ -12,6 +12,19 @@ import java.util.List;
 public class Problem7 {
     public static String solution(List<String> values){
         String answer = "";
+        int max_length = values.stream()
+                .map(String::length)
+                .max(Integer::compare)
+                .get();
+
+        for(int i=0; i<max_length; i++){
+            for(int j=0; j<values.size();j++){
+                if(values.get(j).length()<= i){
+                    continue; //j번째행에서 i번째 글자가 null이면 넘어간다.
+                }
+                answer = answer.concat(values.get(j).substring(i, i + 1));
+            }
+        }
         return answer;
     }
 }
