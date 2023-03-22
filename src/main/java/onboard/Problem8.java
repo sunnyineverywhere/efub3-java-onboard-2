81eb079f71232f83d3d356e7e1df717d0e5b34bd
Minseo Cho <min1130@ewhain.net>
2023-03-23 07:11:24 +0900

    ✨ feat: solve Problem8

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -11,6 +11,27 @@ import java.util.List;
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+
+        int[][] paper = new int[100][100];
+
+        for (int i=0; i<paperNumber; i++) {
+            int x = locations[i][0];
+            int y = locations[i][1];
+
+            for (int j=x; j<x+10; j++) {
+                for (int k=y; k<y+10; k++) {
+                    paper[j][k] = 1;
+                }
+            }
+        }
+
+        for (int i=0; i<100; i++) {
+            for (int j=0; j<100; j++) {
+                if (paper[i][j] == 1)
+                    answer++;
+            }
+        }
+
         return answer;
     }
 }
