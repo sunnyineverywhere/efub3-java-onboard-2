4d7a77b8ee70f0598bc586a53146ee42bee575b4
Min A <min2010011@gmail.com>
2023-03-22 23:16:04 +0900

    feat: Problem8 풀이 및 테스트 케이스 추가

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -1,6 +1,6 @@
 package onboard;
 
-import java.util.List;
+import java.util.Scanner;
 
 /**
  * 백준 2563
@@ -9,8 +9,29 @@ import java.util.List;
  * link : https://www.acmicpc.net/problem/2563
  */
 public class Problem8 {
-    public static Integer solution(Integer paperNumber, Integer[][] locations){
+    public static Integer solution(Integer paperNumber, Integer[][] locations) {
         Integer answer = 0;
+        boolean[][] area = new boolean[101][101];
+
+        for (Integer[] location : locations) {
+            for (int i = location[0]; i < location[0] + 10; i++) {
+                for (int j = location[1]; j < location[1] + 10; j++) {
+                    if (!area[i][j]) answer++;
+                    area[i][j] = true;
+                }
+            }
+        }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int paperNumber = sc.nextInt();
+        Integer[][] locations = new Integer[paperNumber][2];
+        for (int i = 0; i < paperNumber; i++) {
+            locations[i][0] = sc.nextInt();
+            locations[i][1] = sc.nextInt();
+        }
+        System.out.println(Problem8.solution(paperNumber, locations));
     }
 }
--- src/test/java/ProblemTest.java
+++ src/test/java/ProblemTest.java
@@ -10,9 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProblemTest {
     @Nested
-    class Problem6Test{
+    class Problem6Test {
         @Test
-        void case1(){
+        void case1() {
             Integer[][] values = {{0, 1, 0, 1}, {1, 1, 1, 0}, {0, 0, 1, 1}};
             List<String> answer = List.of("B", "A", "B");
             assertThat(Problem6.solution(values)).isEqualTo(answer);
@@ -27,16 +27,16 @@ public class ProblemTest {
     }
 
     @Nested
-    class Problem7Test{
+    class Problem7Test {
         @Test
-        void case1(){
+        void case1() {
             List<String> values = List.of("ABCDE", "abcde", "01234", "FGHIJ", "fghij");
             String answer = "Aa0FfBb1GgCc2HhDd3IiEe4Jj";
             assertThat(Problem7.solution(values)).isEqualTo(answer);
         }
 
         @Test
-        void case2(){
+        void case2() {
             List<String> values = List.of("AABCDD", "afzz", "09121", "a8EWg6", "P5h3kx");
             String answer = "Aa0aPAf985Bz1EhCz2W3D1gkD6x";
             assertThat(Problem7.solution(values)).isEqualTo(answer);
@@ -45,22 +45,30 @@ public class ProblemTest {
         @Test
         void case3() {
             List<String> values = List.of("12345",
-                                          "abcdefg",
-                                          "A",
-                                          "BB",
-                                          "zzzzzzzzzzzzzzz");
+                    "abcdefg",
+                    "A",
+                    "BB",
+                    "zzzzzzzzzzzzzzz");
             String answer = "1aABz2bBz3cz4dz5ezfzgzzzzzzzzz";
             assertThat(Problem7.solution(values)).isEqualTo(answer);
         }
     }
 
     @Nested
-    class Problem8Test{
+    class Problem8Test {
         @Test
-        void case1(){
+        void case1() {
             Integer paperNumber = 3;
             Integer[][] locations = {{3, 7}, {15, 7}, {5, 2}};
             Integer answer = 260;
+            assertThat(Problem8.solution(paperNumber, locations)).isEqualTo(answer);
+        }
+
+        @Test
+        void case2() {
+            Integer paperNumber = 4;
+            Integer[][] locations = {{10, 10}, {11, 11}, {12, 12}, {80, 80}};
+            Integer answer = 238;
             assertThat(Problem8.solution(paperNumber, locations)).isEqualTo(answer);
         }
     }
