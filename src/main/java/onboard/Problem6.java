c3371845ae2ae90724c379dabc6f1ad167f7685a
aoqlsdl <bini92444@gmail.com>
2023-03-23 23:10:23 +0900

    ✨feat: solve problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,24 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+
+        for(int i=0; i<3; i++){
+            Integer counts = 0;
+            for(int j=0; j<4; j++)
+                if(values[i][j]==1) {
+                    counts++;
+                }
+            if (counts == 0)
+                answer.add("D");
+            else if (counts == 1)
+                answer.add("C");
+            else if (counts == 2)
+                answer.add("B");
+            else if (counts == 3)
+                answer.add("A");
+            else
+                answer.add("E");
+        }
         return answer;
     }
 }
