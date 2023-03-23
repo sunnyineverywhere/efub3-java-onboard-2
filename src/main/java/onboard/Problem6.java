309b351551e4147e3afcf2995bcd408f9f91b6aa
choiyounji <yunji71@naver.com>
2023-03-23 22:21:40 +0900

    feat: Problem6 해결

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,22 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            int sum=0;
+            for(int j=0;j<4;j++){
+                sum+=values[i][j];
+            }
+            if(sum==0)
+                answer.add("D");
+            else if(sum==1)
+                answer.add("C");
+            else if(sum==2)
+                answer.add("B");
+            else if(sum==3)
+                answer.add("A");
+            else
+                answer.add("E");
+        }
         return answer;
     }
 }
