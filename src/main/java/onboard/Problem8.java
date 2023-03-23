954402a58c81aac201346d0bb184f829bcde7dd2
aqswa <joayoung3@gmail.com>
2023-03-23 16:30:40 +0900

    ✅ test: problem8

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -2,15 +2,17 @@ package onboard;
 
 import java.util.List;
 
-/**
- * 백준 2563
- * name: 색종이
- * 실버 5
- * link : https://www.acmicpc.net/problem/2563
- */
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+        answer += paperNumber * 100;
+        for(int i=0; i<paperNumber; i++){
+            for(int j=i+1; j<paperNumber; j++){
+                if(Math.abs(locations[i][0]-locations[j][0]) < 10 && Math.abs(locations[i][1]-locations[j][1]) < 10){
+                    answer -= (10-Math.abs(locations[i][0]-locations[j][0])) * (10-Math.abs(locations[i][1]-locations[j][1]));
+                }
+            }
+        }
         return answer;
     }
 }
