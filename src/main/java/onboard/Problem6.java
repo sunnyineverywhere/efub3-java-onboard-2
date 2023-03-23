746d6011d0fbda0d920ca273a5720be37a22e9cf
aqswa <joayoung3@gmail.com>
2023-03-23 12:20:53 +0900

    ✅ test: problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -3,15 +3,35 @@ package onboard;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 백준 2490
- * name: 윷놀이
- * 브론즈 3
- * link : https://www.acmicpc.net/problem/2490
- */
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            int cnt = 0;
+            for(int j=0; j<4; j++){
+                if(values[i][j] == 1){
+                    cnt++;
+                }
+            }
+
+            switch(cnt){
+                case 3:
+                    answer.add("A");
+                    break;
+                case 2:
+                    answer.add("B");
+                    break;
+                case 1:
+                    answer.add("C");
+                    break;
+                case 0:
+                    answer.add("D");
+                    break;
+                case 4:
+                    answer.add("E");
+                    break;
+            }
+        }
         return answer;
     }
 }
