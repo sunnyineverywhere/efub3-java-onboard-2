35a9293b8d268afde4515ee69739bc9f61297cee
aqswa <joayoung3@gmail.com>
2023-03-23 13:59:50 +0900

    ✅ test: problem7

--- src/main/java/onboard/Problem7.java
+++ src/main/java/onboard/Problem7.java
@@ -3,15 +3,16 @@ package onboard;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 백준 10798
- * name: 세로읽기
- * 브론즈 1
- * link : https://www.acmicpc.net/problem/10798
- */
 public class Problem7 {
     public static String solution(List<String> values){
         String answer = "";
+        for(int i=1; i<=15; i++){
+            for(int j=0; j<values.size(); j++){
+                if(values.get(j).length() >= i){
+                    answer += values.get(j).charAt(i-1);
+                }
+            }
+        }
         return answer;
     }
 }
