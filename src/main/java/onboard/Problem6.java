2b25e28f848578b5172eeee809f3385e01833d43
Beanii <chlqls212@gmail.com>
2023-03-23 14:20:13 +0900

    ✨ feat: solve Problem6

--- src/main/java/onboard/Problem6.java
+++ src/main/java/onboard/Problem6.java
@@ -12,6 +12,30 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(Integer[][] values){
         List<String> answer = new ArrayList<>();
+        for(int i = 0; i < values.length; i++) {
+            int cnt = 0;
+            for(int j = 0; j < values[i].length; j++) {
+                if(values[i][j] == 0)
+                    cnt++;
+            }
+            switch (cnt){
+                case 0:
+                    answer.add("E");
+                    break;
+                case 1:
+                    answer.add("A");
+                    break;
+                case 2:
+                    answer.add("B");
+                    break;
+                case 3:
+                    answer.add("C");
+                    break;
+                case 4:
+                    answer.add("D");
+                    break;
+            }
+        }
         return answer;
     }
 }
