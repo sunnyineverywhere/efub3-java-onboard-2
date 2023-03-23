38dbc617fe67a48eaa75b5ee3bdc25d1900f833b
choiyounji <yunji71@naver.com>
2023-03-23 23:19:30 +0900

    feat: Problem7 해결

--- src/main/java/onboard/Problem7.java
+++ src/main/java/onboard/Problem7.java
@@ -12,6 +12,24 @@ import java.util.List;
 public class Problem7 {
     public static String solution(List<String> values){
         String answer = "";
+        int max=0;
+        for(int i=0;i<5;i++){
+            if(values.get(i).length()>max)
+                max=values.get(i).length();
+        }
+        char[][] newvalues=new char[5][max];
+        for(int i=0;i<5;i++){
+            for(int j=0;j<values.get(i).length();j++){
+                newvalues[i][j]=values.get(i).charAt(j);
+            }
+        }
+        for(int i=0;i<max;i++){
+            for(int j=0;j<5;j++){
+                if(newvalues[j][i]=='\0')
+                    continue;
+                answer+=newvalues[j][i];
+            }
+        }
         return answer;
     }
 }
