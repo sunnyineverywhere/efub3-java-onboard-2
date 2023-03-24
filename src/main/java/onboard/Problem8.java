d0d04f4bdb1c0a72382be4ca49ce1f0b4076fc18
choiyounji <yunji71@naver.com>
2023-03-24 21:07:17 +0900

    feat: Problem8 해결

--- src/main/java/onboard/Problem8.java
+++ src/main/java/onboard/Problem8.java
@@ -11,6 +11,17 @@ import java.util.List;
 public class Problem8 {
     public static Integer solution(Integer paperNumber, Integer[][] locations){
         Integer answer = 0;
+        boolean[][] arr=new boolean[100][100];
+        for(int i=0;i<paperNumber;i++){
+            for(int j=locations[i][0];j<locations[i][0]+10;j++){
+                for(int k=locations[i][1];k<locations[i][1]+10;k++){
+                    if(!arr[j][k]) {
+                        answer++;
+                        arr[j][k]=true;
+                    }
+                }
+            }
+        }
         return answer;
     }
 }
